f9149703d851a4d360402f9e8d693b3735e903ef
hasan-nazmul <nh2826239@gmail.com>
2025-10-05 19:33:15 +0600

    graph solutions

--- cses/graph/Round_Trip.cpp
+++ cses/graph/Round_Trip.cpp
@@ -1,125 +1,83 @@
-/// Bismillahir Rahmanir Rahim
-
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define max3(a, b, c) max(max(a, b), c)
+#define max4(a, b, c, d) max(max(a, b), max(c, d))
+#define fr(i, n) for (ll i = 0; i < n; i++)
 
-#define ll long long
-#define nl '\n'
-#define pr pair <ll, ll>
-#define sp ' '
-#define mod(a, b) (((a) % (b)) + (b)) % (b)
-#define sz(v) (int) v.size()
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define YES cout << "YES" << nl
-#define NO cout << "NO" << nl
-#define Yes cout << "Yes" << nl
-#define No cout << "No" << nl
-#define yes cout << "yes" << nl
-#define no cout << "no" << nl
-#define neg cout << -1 << nl
-#define OUT(v, n) for(auto x: v) cout << x << n;
-#define PAIR_OUT(v) for(auto x: v) \
-        cout << x.first << " " << x.second << nl;
-#define eps 1000000007
+const int MAXN = 100005;
+ll parent[MAXN];
+ll color[MAXN];
+vector<vector<ll>> adj(MAXN);
+vector<ll> cycle;
 
-const int mx_sz = 1e5+10;
-
-int n, m;
-
-vector <ll> adj[mx_sz], last(mx_sz, -1), ans;
-
-int dfs(int source, int val)
-{
-    if(last[source]!=-1)    
-    {
-        if(val-last[source]>=3)
-        {
-            ans.push_back(source);
-            return source;
-        }
-        
-        return -1;
-    }
+bool dfs(ll node, ll par) {
+    color[node] = 1; // Mark as visiting (gray)
+    parent[node] = par;
     
-    last[source] = val;
-    
-    for(auto i : adj[source])
-    {
-        int f = dfs(i, val+1);
+    for (auto child : adj[node]) {
+        if (child == par) continue; // Skip parent
         
-        if(f!=-1)
-        {
-            ans.push_back(source);
-            
-            if(ans.back()==ans.front())
-                return -1;
-            else    return source;
-        }
-    }
-
-    // last[source] = -1;
-
-    return -1;
-}
-
-void solve()
-{
-    cin >> n >> m;
-
-    for(int i=0; i<m; i++)
-    {
-        ll u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    for(int i=1; i<=n; i++)
-    {
-        if(last[i] != -1)   continue;
-        ans.clear();
-        dfs(i, 1);
-        if(sz(ans)>3) 
-        {
-            int idx=-1;
-
-            for(int i=1; i<sz(ans); i++)
-            {
-                if(ans[i]==ans[0])
-                {
-                    idx = i;
-                    break;
-                }
+        if (color[child] == 0) { // Unvisited node (white)
+            if (dfs(child, node)) {
+                return true;
             }
-
-            cout << idx+1 << nl;
-
-            for(int i=0; i<=idx; i++)
-                cout << ans[i] << sp;
-
-            cout << nl;
-
-            return;
+        } 
+        else if (color[child] == 1) { // Back edge found - cycle detected
+            // Reconstruct the cycle
+            cycle.push_back(child);
+            ll cur = node;
+            while (cur != child) {
+                cycle.push_back(cur);
+                cur = parent[cur];
+            }
+            cycle.push_back(child); // Close the cycle
+            return true;
         }
     }
-
-    cout << "IMPOSSIBLE" << nl;
-
-    return;
+    
+    color[node] = 2; // Mark as fully processed (black)
+    return false;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-    solve();
-
-    // ll M; cin >> M;
-
-    // while(M--) {
-    //    solve();
-    // }
-
+int main() {
+    fastio;
+    ll n, m;
+    cin >> n >> m;
+    
+    // Initialize arrays
+    memset(color, 0, sizeof(color));
+    memset(parent, -1, sizeof(parent));
+    
+    // Build graph
+    while (m--) {
+        ll x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    
+    // Perform DFS from each unvisited node
+    for (ll i = 1; i <= n; i++) {
+        if (color[i] == 0) {
+            if (dfs(i, -1)) {
+                // Cycle found - print it
+                cout << cycle.size() << "\n";
+                for (auto node : cycle) {
+                    cout << node << " ";
+                }
+                cout << "\n";
+                return 0;
+            }
+        }
+    }
+    
+    // No cycle found
+    cout << "IMPOSSIBLE\n";
     return 0;
 }
