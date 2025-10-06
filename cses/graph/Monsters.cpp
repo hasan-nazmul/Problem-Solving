27634e031cfdc0a76e7f1065131e662ed48332de
hasan-nazmul <nh2826239@gmail.com>
2025-10-06 20:27:41 +0600

    Monsters Solve

--- cses/graph/Monsters.cpp
+++ cses/graph/Monsters.cpp
@@ -1,46 +1,48 @@
 /// Bismillahir Rahmanir Rahim
-
+ 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define ll long long
 #define nl '\n'
 #define pr pair <ll, ll>
 #define sp ' '
 #define mod(a, b) (((a) % (b)) + (b)) % (b)
 #define sz(v) (int) v.size()
-
+ 
 const int mx = 1e3+10;
-
+ 
 int n, m;
-
+ 
 string grid[mx];
-
-int dist[mx][mx], path[mx][mx];
-
+ 
+int dist[mx][mx], level[mx][mx];
+char path[mx][mx];
+ 
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
+ 
     memset(dist, -1, sizeof(dist));
-    memset(path, -1, sizeof(path));
-
+    memset(level, -1, sizeof(level));
+    memset(path, 'x', sizeof(path));
+ 
     cin >> n >> m;
-
+ 
     for(int i=1; i<=n; i++)
     {
         string s;
         cin >> s;
         grid[i] = '#' + s;
     }
-
+ 
     auto valid_idx = [&](int x, int y)
     {
         return x > 0 and x <= n and y > 0 and y <= m;
     };
-
+ 
     queue <pair <int, int>> bfs;
-
+ 
     for(int i=1; i<=n; i++)
     {
         for(int j=1; j<=m; j++)
@@ -52,31 +54,32 @@ int main()
             }
         }
     }
-
+ 
     int dx[] = {0, 0, +1, -1};
     int dy[] = {+1, -1, 0, 0};
-
+    string dir = "RLDU";
+ 
     while(!bfs.empty())
     {
         auto curr = bfs.front();
         bfs.pop();
-
+ 
         for(int i=0; i<4; i++)
         {
             int nx = curr.first + dx[i];
             int ny = curr.second + dy[i];
-
-            if(!valid_idx(nx, ny) or dist[nx][ny]!=-1)  
+ 
+            if(!valid_idx(nx, ny) or dist[nx][ny]!=-1)
                 continue;
-
+ 
             else if(valid_idx(nx, ny) and grid[nx][ny]=='#')
                 continue;
-
+ 
             bfs.push({nx,ny});
             dist[nx][ny] = dist[curr.first][curr.second] + 1;
         }
     }
-
+ 
     for(int i=1; i<=n; i++)
     {
         for(int j=1; j<=m; j++)
@@ -84,58 +87,84 @@ int main()
             if(grid[i][j]=='A')
             {
                 bfs.push({i,j});
-                path[i][j] = 0;
+                level[i][j] = 0;
             }
         }
     }
-
+ 
     bool found = 0;
-
+ 
     pair <int, int> it;
-
+ 
     while(!bfs.empty())
     {
         auto curr = bfs.front();
         bfs.pop();
-
-        if(!curr.first or !curr.second or curr.first==n or curr.second==m)
+ 
+        if(curr.first==1 or curr.second==1 or curr.first==n or curr.second==m)
         {
             found = 1;
             it = curr;
             break;
         }
-
+ 
         for(int i=0; i<4; i++)
         {
             int nx = curr.first + dx[i];
             int ny = curr.second + dy[i];
-
-            if(!valid_idx(nx, ny) or path[nx][ny]!=-1)
+ 
+            if(!valid_idx(nx, ny) or level[nx][ny]!=-1)
                 continue;
-
+ 
             else if(valid_idx(nx, ny))
             {
-                if(grid[nx][ny]!='.' or dist[nx][ny]<=path[curr.first][curr.second]+1)
+                if(grid[nx][ny]!='.' or (dist[nx][ny]!=-1 and dist[nx][ny]<=level[curr.first][curr.second]+1))
                     continue;
             }
-
+ 
             bfs.push({nx,ny});
-            path[nx][ny] = path[curr.first][curr.second] + 1;
+            level[nx][ny] = level[curr.first][curr.second] + 1;
+            path[nx][ny] = dir[i];
         }
     }
-
+ 
     if(!found)
     {
         cout << "NO" << nl;
-        return;
+        return 0;
     }
-
+ 
     string ans;
-
-    while(path[it.first][it.second])
+ 
+    while(grid[it.first][it.second]!='A')
     {
-
+        ans.push_back(path[it.first][it.second]);
+ 
+        switch(ans.back())
+        {
+ 
+        case 'U':
+            it.first++;
+            break;
+        case 'D':
+            it.first--;
+            break;
+        case 'L':
+            it.second++;
+            break;
+        case 'R':
+            it.second--;
+            break;
+        default:
+            break;
+        }
     }
-
+ 
+    cout << "YES\n" << sz(ans) << nl;
+ 
+    reverse(ans.begin(), ans.end());
+ 
+    cout << ans << nl;
+ 
     return 0;
 }
